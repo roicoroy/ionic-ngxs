0683cf0332a41140582ef2ff713cb77c3dc1ff32
BentoRi <bentor@emea.cshare.net>
2022-01-27 21:14:24 +0000

    z

--- src/app/calculator-ngxs/calculator-ngxs.page.ts
+++ src/app/calculator-ngxs/calculator-ngxs.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-calculator-ngxs',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorNgxsPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
   }
-
+  home() {
+    this.navCtrl.navigateForward('home');
+  }
 }
